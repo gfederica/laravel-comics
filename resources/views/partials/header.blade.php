240eb7f0537c1ac513288f7a5b3417fa68578320
gfederica <effegiudice@gmail.com>
2021-07-13 17:09:15 +0200

    inizio header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,12 @@
+<header>
+    <div class="container">
+        <div class="pre-header">
+            <ul>
+                <li><a href="#">DC POWER℠ VISA®</a></li>
+                <li><a href="#">ADDITIONAL DC SITES<i class="fas fa-caret-down"></i></a></li>
+            </ul>
+        </div>
+        <div class="big-header">
+        </div>
+    </div>
+</header>
